e2d929aa00ff1550081726d105c31b52ed5c0cb2
Sitting Fox <SittingFox@users.noreply.github.com>
2015-05-10 23:31:50 -0400

    feat: Add basic Enemy and Player objects
    This adds Enemy and Player objects, and some other related bits, giving just
    enough to display them on screen without errors.

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+    
+    this.x = 100;
+    this.y = 100;
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,24 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+	Enemy.call(this);
+	this.sprite = 'images/char-boy.png';
+};
 
+Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.constructor = Player;
+
+Player.prototype.handleInput = function(key) {
+	
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var allEnemies = [new Enemy()];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
